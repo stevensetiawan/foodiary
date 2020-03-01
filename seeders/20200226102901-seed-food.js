a4817fd6da9bfd579551064c069e962f19a61a7e
adhiyatmarama <adhiyatma.pramayoga@gmail.com>
2020-03-01 08:45:15 +0700

    nambah image di seed food

--- seeders/20200226102901-seed-food.js
+++ seeders/20200226102901-seed-food.js
@@ -15,31 +15,43 @@ module.exports = {
             calories: 333,
             createdAt: new Date(),
             updatedAt: new Date(),
+            image: '/nasi goreng.jpeg'
         },
         {
           name: 'Mie Ayam',
           calories: 421,
           createdAt: new Date(),
           updatedAt: new Date(),
+          image: '/mie-ayam.jpg'
         },
         {
           name: 'Bakso',
           calories: 325,
           createdAt: new Date(),
           updatedAt: new Date(),
+          image: '/bakso.jpg'
         },
         {
           name: 'Sate Ayam',
           calories: 681,
           createdAt: new Date(),
           updatedAt: new Date(),
+          image: '/sate-ayam.jpg'
         },
         {
           name: 'Soto Ayam',
           calories: 321,
           createdAt: new Date(),
           updatedAt: new Date(),
-        }
+          image: '/soto-ayam.jpeg'
+        },
+        {
+          name: 'Ketoprak',
+          calories: 402,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          image: '/ketoprak.jpeg'
+        }      
       ], {});
     },
 
